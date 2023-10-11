5fc5db03183b087d8bf06564f6beb392be6a90f5
codeStackr <rdzaman20000@gmail.com>
2023-10-12 05:06:38 +0600

    Fix data type of name

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS public.animals
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    name "char",
+    name "varchar",
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
